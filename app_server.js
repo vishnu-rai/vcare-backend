5fce04161ac1019cff44df05922f0fe880b4d21d
Vishnu1999 <Vishnurai1999@gmail.com>
2021-11-27 01:19:00 +0530

    Email otp verification.

--- app_server.js
+++ app_server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const bcrypt = require("bcrypt");
 const mysql = require("mysql");
-// const generateAccessToken = require("./generateAccessToken");
 var emailvalidator = require("email-validator");
 
 var app = express();
--- generateAccessToken.js
+++
@@ -1,5 +0,0 @@
-const jwt = require("jsonwebtoken");
-function generateAccessToken(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
-}
-module.exports = generateAccessToken;
