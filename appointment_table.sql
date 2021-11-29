537de2f7b932c4b74d3c5427a451fd2f34a3e9c1
Vishnu1999 <Vishnurai1999@gmail.com>
2021-11-29 22:19:18 +0530

    env file

--- appointment_table.sql
+++ appointment_table.sql
@@ -1,16 +1,16 @@
 use project1;
 
-/*create table appointment_table (
-  appointment_id int primary key not null unique,
-  user_id int not null,
-  verification_status boolean not null default false,
-  hospital_id int not null,
-  doctor_id int not null,
-  department varchar(25) not null,
-  appointment_date date not null,
-  appointment_number int not null
-  );*/
+-- create table appointment_table (
+--   appointment_id int primary key not null unique default 1,
+--   user_id int not null,
+--   verification_status boolean not null default false,
+--   hospital_id int not null,
+--   doctor_id int not null,
+--   department varchar(25) not null,
+--   appointment_date date not null,
+--   appointment_number int not null
+--   );
   
---   delete from appointment_table where appointment_id=0;
+-- delete from appointment_table where appointment_id=0;
 
 select * from appointment_table;
